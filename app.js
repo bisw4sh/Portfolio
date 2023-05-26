31c2c4e193315c7a6f79d8fa6002f630717e9bb5
Biswash <59287553+bisw4sh@users.noreply.github.com>
2023-05-26 16:31:23 +0545

    check point

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-document.body.style.color ='white';
+// (function () {
+//     [...document.querySelectorAll(".control")].forEach(button => {
+//         button.addEventListener("click", function() {
+//             document.querySelector(".active-btn").classList.remove("active-btn");
+//             this.classList.add("active-btn");
+//             document.querySelector(".active").classList.remove("active");
+//             document.getElementById(button.dataset.id).classList.add("active");
+//         })
+//     });
+//     document.querySelector(".theme-btn").addEventListener("click", () => {
+//         document.body.classList.toggle("light-mode");
+//     })
+// })();
